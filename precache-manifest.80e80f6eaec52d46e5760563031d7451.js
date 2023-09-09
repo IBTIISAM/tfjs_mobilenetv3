bad396677d03dc3a27fa599f7f05d57432689e4c
IBTIISAM <ibtesam-almohsen@hotmail.com>
2023-09-10 00:45:40 +0300

    Updates

---
+++ precache-manifest.80e80f6eaec52d46e5760563031d7451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "819f7c4522c855982803",
+    "url": "/tfjs_mobilenetv3/static/css/main.99cbe6c1.chunk.css"
+  },
+  {
+    "revision": "819f7c4522c855982803",
+    "url": "/tfjs_mobilenetv3/static/js/main.819f7c45.chunk.js"
+  },
+  {
+    "revision": "ebf1d4527d3b7f726ecf",
+    "url": "/tfjs_mobilenetv3/static/js/1.ebf1d452.chunk.js"
+  },
+  {
+    "revision": "4e426a638f2518a9ff94",
+    "url": "/tfjs_mobilenetv3/static/js/2.4e426a63.chunk.js"
+  },
+  {
+    "revision": "0fa1f6a288ad6978b67a",
+    "url": "/tfjs_mobilenetv3/static/js/runtime~main.0fa1f6a2.js"
+  },
+  {
+    "revision": "de5209d87783348e7334a51e13c04022",
+    "url": "/tfjs_mobilenetv3/index.html"
+  }
+];
